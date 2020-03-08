8cbe4bd93d64d10b008191963d3ad5ff59edbb97
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-03-08 18:07:48 -0400

    got scan

--- test/storage/bplustree_index_test.cpp
+++ test/storage/bplustree_index_test.cpp
@@ -36,7 +36,7 @@ class BPlusTreeIndexTests : public TerrierTest {
  public:
   std::default_random_engine generator_;
   const uint32_t num_threads_ = 12;
-  const uint32_t num_inserts_ = 10000000;  // number of tuples/primary keys for each worker to attempt to insert
+  const uint32_t num_inserts_ = 1000;  // number of tuples/primary keys for each worker to attempt to insert
 
   std::unique_ptr<DBMain> db_main_;
   common::ManagedPointer<transaction::TransactionManager> txn_manager_;
@@ -46,7 +46,7 @@ class BPlusTreeIndexTests : public TerrierTest {
   storage::ProjectedRowInitializer tuple_initializer_ =
       storage::ProjectedRowInitializer::Create(std::vector<uint16_t>{1}, std::vector<uint16_t>{1});
 
-  // BwTreeIndex
+  // BPlusTreeIndex
   Index *default_index_, *unique_index_;
 
   byte *key_buffer_1_, *key_buffer_2_;
@@ -562,6 +562,222 @@ TEST_F(BPlusTreeIndexTests, CommitInsert2) {
   results.clear();
 
   txn_manager_->Commit(txn2, transaction::TransactionUtil::EmptyCallback, nullptr);
+}
+
+/**
+ * Tests basic scan behavior using various windows to scan over (some out of of bounds of keyspace, some matching
+ * exactly, etc.)
+ */
+// NOLINTNEXTLINE
+TEST_F(BPlusTreeIndexTests, ScanAscending) {
+  // populate index with [0..20] even keys
+  std::map<int32_t, storage::TupleSlot> reference;
+  auto *const insert_txn = txn_manager_->BeginTransaction();
+  for (int32_t i = 0; i <= 20; i += 2) {
+    auto *const insert_redo =
+        insert_txn->StageWrite(CatalogTestUtil::TEST_DB_OID, CatalogTestUtil::TEST_TABLE_OID, tuple_initializer_);
+    auto *const insert_tuple = insert_redo->Delta();
+    *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+    const auto tuple_slot = sql_table_->Insert(common::ManagedPointer(insert_txn), insert_redo);
+
+    auto *const insert_key = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+    *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+
+    EXPECT_TRUE(default_index_->Insert(common::ManagedPointer(insert_txn), *insert_key, tuple_slot));
+    reference[i] = tuple_slot;
+  }
+  txn_manager_->Commit(insert_txn, transaction::TransactionUtil::EmptyCallback, nullptr);
+
+  auto *const scan_txn = txn_manager_->BeginTransaction();
+
+  std::vector<storage::TupleSlot> results;
+
+  auto *const low_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+  auto *const high_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_2_);
+
+  // scan[8,12] should hit keys 8, 10, 12
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 8;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 12;
+  default_index_->ScanAscending(*scan_txn, storage::index::ScanType::Closed, 1, low_key_pr, high_key_pr, 0, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(8), results[0]);
+  EXPECT_EQ(reference.at(10), results[1]);
+  EXPECT_EQ(reference.at(12), results[2]);
+  results.clear();
+
+  // scan[7,13] should hit keys 8, 10, 12
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 7;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 13;
+  default_index_->ScanAscending(*scan_txn, storage::index::ScanType::Closed, 1, low_key_pr, high_key_pr, 0, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(8), results[0]);
+  EXPECT_EQ(reference.at(10), results[1]);
+  EXPECT_EQ(reference.at(12), results[2]);
+  results.clear();
+
+  // scan[-1,5] should hit keys 0, 2, 4
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = -1;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 5;
+  default_index_->ScanAscending(*scan_txn, storage::index::ScanType::Closed, 1, low_key_pr, high_key_pr, 0, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(0), results[0]);
+  EXPECT_EQ(reference.at(2), results[1]);
+  EXPECT_EQ(reference.at(4), results[2]);
+  results.clear();
+
+  // scan[15,21] should hit keys 16, 18, 20
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 15;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 21;
+  default_index_->ScanAscending(*scan_txn, storage::index::ScanType::Closed, 1, low_key_pr, high_key_pr, 0, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(16), results[0]);
+  EXPECT_EQ(reference.at(18), results[1]);
+  EXPECT_EQ(reference.at(20), results[2]);
+  results.clear();
+
+  txn_manager_->Commit(scan_txn, transaction::TransactionUtil::EmptyCallback, nullptr);
+}
+
+/**
+ * Tests basic scan behavior using various windows to scan over (some out of of bounds of keyspace, some matching
+ * exactly, etc.)
+ */
+// NOLINTNEXTLINE
+TEST_F(BPlusTreeIndexTests, ScanDescending) {
+  // populate index with [0..20] even keys
+  std::map<int32_t, storage::TupleSlot> reference;
+  auto *const insert_txn = txn_manager_->BeginTransaction();
+  for (int32_t i = 0; i <= 20; i += 2) {
+    auto *const insert_redo =
+        insert_txn->StageWrite(CatalogTestUtil::TEST_DB_OID, CatalogTestUtil::TEST_TABLE_OID, tuple_initializer_);
+    auto *const insert_tuple = insert_redo->Delta();
+    *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+    const auto tuple_slot = sql_table_->Insert(common::ManagedPointer(insert_txn), insert_redo);
+
+    auto *const insert_key = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+    *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+    EXPECT_TRUE(default_index_->Insert(common::ManagedPointer(insert_txn), *insert_key, tuple_slot));
+    reference[i] = tuple_slot;
+  }
+  txn_manager_->Commit(insert_txn, transaction::TransactionUtil::EmptyCallback, nullptr);
+
+  auto *const scan_txn = txn_manager_->BeginTransaction();
+
+  std::vector<storage::TupleSlot> results;
+
+  auto *const low_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+  auto *const high_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_2_);
+
+  // scan[8,12] should hit keys 12, 10, 8
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 8;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 12;
+  default_index_->ScanDescending(*scan_txn, *low_key_pr, *high_key_pr, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(12), results[0]);
+  EXPECT_EQ(reference.at(10), results[1]);
+  EXPECT_EQ(reference.at(8), results[2]);
+  results.clear();
+
+  // scan[7,13] should hit keys 12, 10, 8
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 7;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 13;
+  default_index_->ScanDescending(*scan_txn, *low_key_pr, *high_key_pr, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(12), results[0]);
+  EXPECT_EQ(reference.at(10), results[1]);
+  EXPECT_EQ(reference.at(8), results[2]);
+  results.clear();
+
+  // scan[-1,5] should hit keys 4, 2, 0
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = -1;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 5;
+  default_index_->ScanDescending(*scan_txn, *low_key_pr, *high_key_pr, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(4), results[0]);
+  EXPECT_EQ(reference.at(2), results[1]);
+  EXPECT_EQ(reference.at(0), results[2]);
+  results.clear();
+
+  // scan[15,21] should hit keys 20, 18, 16
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 15;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 21;
+  default_index_->ScanDescending(*scan_txn, *low_key_pr, *high_key_pr, &results);
+  EXPECT_EQ(results.size(), 3);
+  EXPECT_EQ(reference.at(20), results[0]);
+  EXPECT_EQ(reference.at(18), results[1]);
+  EXPECT_EQ(reference.at(16), results[2]);
+  results.clear();
+
+  txn_manager_->Commit(scan_txn, transaction::TransactionUtil::EmptyCallback, nullptr);
+}
+
+/**
+ * Tests basic scan behavior using various windows to scan over (some out of of bounds of keyspace, some matching
+ * exactly, etc.)
+ */
+// NOLINTNEXTLINE
+TEST_F(BPlusTreeIndexTests, ScanLimitDescending) {
+  // populate index with [0..20] even keys
+  std::map<int32_t, storage::TupleSlot> reference;
+  auto *const insert_txn = txn_manager_->BeginTransaction();
+  for (int32_t i = 0; i <= 20; i += 2) {
+    auto *const insert_redo =
+        insert_txn->StageWrite(CatalogTestUtil::TEST_DB_OID, CatalogTestUtil::TEST_TABLE_OID, tuple_initializer_);
+    auto *const insert_tuple = insert_redo->Delta();
+    *reinterpret_cast<int32_t *>(insert_tuple->AccessForceNotNull(0)) = i;
+    const auto tuple_slot = sql_table_->Insert(common::ManagedPointer(insert_txn), insert_redo);
+
+    auto *const insert_key = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+    *reinterpret_cast<int32_t *>(insert_key->AccessForceNotNull(0)) = i;
+    EXPECT_TRUE(default_index_->Insert(common::ManagedPointer(insert_txn), *insert_key, tuple_slot));
+    reference[i] = tuple_slot;
+  }
+  txn_manager_->Commit(insert_txn, transaction::TransactionUtil::EmptyCallback, nullptr);
+
+  auto *const scan_txn = txn_manager_->BeginTransaction();
+
+  std::vector<storage::TupleSlot> results;
+
+  auto *const low_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_1_);
+  auto *const high_key_pr = default_index_->GetProjectedRowInitializer().InitializeRow(key_buffer_2_);
+
+  // scan_limit[8,12] should hit keys 12, 10
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 8;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 12;
+  default_index_->ScanLimitDescending(*scan_txn, *low_key_pr, *high_key_pr, &results, 2);
+  EXPECT_EQ(results.size(), 2);
+  EXPECT_EQ(reference.at(12), results[0]);
+  EXPECT_EQ(reference.at(10), results[1]);
+  results.clear();
+
+  // scan_limit[7,13] should hit keys 12, 10
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 7;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 13;
+  default_index_->ScanLimitDescending(*scan_txn, *low_key_pr, *high_key_pr, &results, 2);
+  EXPECT_EQ(results.size(), 2);
+  EXPECT_EQ(reference.at(12), results[0]);
+  EXPECT_EQ(reference.at(10), results[1]);
+  results.clear();
+
+  // scan_limit[-1,5] should hit keys 4, 2
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = -1;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 5;
+  default_index_->ScanLimitDescending(*scan_txn, *low_key_pr, *high_key_pr, &results, 2);
+  EXPECT_EQ(results.size(), 2);
+  EXPECT_EQ(reference.at(4), results[0]);
+  EXPECT_EQ(reference.at(2), results[1]);
+  results.clear();
+
+  // scan_limit[15,21] should hit keys 20, 18
+  *reinterpret_cast<int32_t *>(low_key_pr->AccessForceNotNull(0)) = 15;
+  *reinterpret_cast<int32_t *>(high_key_pr->AccessForceNotNull(0)) = 21;
+  default_index_->ScanLimitDescending(*scan_txn, *low_key_pr, *high_key_pr, &results, 2);
+  EXPECT_EQ(results.size(), 2);
+  EXPECT_EQ(reference.at(20), results[0]);
+  EXPECT_EQ(reference.at(18), results[1]);
+  results.clear();
+
+  txn_manager_->Commit(scan_txn, transaction::TransactionUtil::EmptyCallback, nullptr);
 }
 
 }  // namespace terrier::storage::index
