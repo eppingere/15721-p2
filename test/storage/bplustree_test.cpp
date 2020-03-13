c0de8d56f09a863768fa79f9af4c50893a609323
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-03-13 18:28:04 -0400

    wrote most of allocator and gc

--- test/storage/bplustree_test.cpp
+++ test/storage/bplustree_test.cpp
@@ -23,18 +23,20 @@ TEST_F(BPlusTreeTests, CompressTest1) {
   uint64_t start_depth = test_tree_->GetDepth();
 
   uint64_t num_start_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_start_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_start_leaves++;
 
   for (uint64_t j = 0; j < num_inserts_; j++) {
-    EXPECT_TRUE(test_tree_->Remove(j,j));
+    EXPECT_TRUE(test_tree_->Remove(j, j));
   }
 
   uint64_t del_depth = test_tree_->GetDepth();
 
   uint64_t num_end_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_end_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_end_leaves++;
 
   EXPECT_EQ(num_start_leaves, num_end_leaves);
   EXPECT_EQ(start_depth, del_depth);
@@ -44,11 +46,12 @@ TEST_F(BPlusTreeTests, CompressTest1) {
   uint64_t compress_depth = test_tree_->GetDepth();
 
   uint64_t compressed_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    compressed_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    compressed_leaves++;
 
   EXPECT_EQ(compress_depth, start_depth);
-  EXPECT_EQ(compressed_leaves, num_start_leaves / (BPlusTree<uint64_t, uint64_t >::LEAF_SIZE / 2));
+  EXPECT_EQ(compressed_leaves, num_start_leaves / (BPlusTree<uint64_t, uint64_t>::LEAF_SIZE / 2));
 }
 
 TEST_F(BPlusTreeTests, CompressTest2) {
@@ -61,18 +64,20 @@ TEST_F(BPlusTreeTests, CompressTest2) {
   uint64_t start_depth = test_tree_->GetDepth();
 
   uint64_t num_start_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_start_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_start_leaves++;
 
   for (uint64_t j = 0; j < num_inserts_; j += 2) {
-    EXPECT_TRUE(test_tree_->Remove(j,j));
+    EXPECT_TRUE(test_tree_->Remove(j, j));
   }
 
   uint64_t del_depth = test_tree_->GetDepth();
 
   uint64_t num_end_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_end_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_end_leaves++;
 
   EXPECT_EQ(num_start_leaves, num_end_leaves);
   EXPECT_EQ(start_depth, del_depth);
@@ -81,12 +86,11 @@ TEST_F(BPlusTreeTests, CompressTest2) {
 
   std::vector<uint64_t> results;
   for (uint64_t i = 1; i < num_inserts_; i += 2) {
-    test_tree_->ScanKey(i, &results, [] (uint64_t val) { return true; });
+    test_tree_->ScanKey(i, &results, [](uint64_t val) { return true; });
     EXPECT_EQ(results.size(), 1);
     EXPECT_EQ(results[0], i);
     results.clear();
   }
-
 }
 
 TEST_F(BPlusTreeTests, CompressTest3) {
@@ -99,18 +103,20 @@ TEST_F(BPlusTreeTests, CompressTest3) {
   uint64_t start_depth = test_tree_->GetDepth();
 
   uint64_t num_start_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_start_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_start_leaves++;
 
   for (uint64_t j = 0; j < BPlusTree<uint64_t, uint64_t>::LEAF_SIZE + 1; j += 2) {
-    EXPECT_TRUE(test_tree_->Remove(j,j));
+    EXPECT_TRUE(test_tree_->Remove(j, j));
   }
 
   uint64_t del_depth = test_tree_->GetDepth();
 
   uint64_t num_end_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_end_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_end_leaves++;
 
   EXPECT_EQ(num_start_leaves, num_end_leaves);
   EXPECT_EQ(start_depth, del_depth);
@@ -119,12 +125,11 @@ TEST_F(BPlusTreeTests, CompressTest3) {
 
   std::vector<uint64_t> results;
   for (uint64_t i = 1; i < BPlusTree<uint64_t, uint64_t>::LEAF_SIZE + 1; i += 2) {
-    test_tree_->ScanKey(i, &results, [] (uint64_t val) { return true; });
+    test_tree_->ScanKey(i, &results, [](uint64_t val) { return true; });
     EXPECT_EQ(results.size(), 1);
     EXPECT_EQ(results[0], i);
     results.clear();
   }
-
 }
 
 TEST_F(BPlusTreeTests, CompressTest4) {
@@ -137,18 +142,20 @@ TEST_F(BPlusTreeTests, CompressTest4) {
   uint64_t start_depth = test_tree_->GetDepth();
 
   uint64_t num_start_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_start_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_start_leaves++;
 
   for (uint64_t j = 0; j < BPlusTree<uint64_t, uint64_t>::LEAF_SIZE / 2; j += 2) {
-    EXPECT_TRUE(test_tree_->Remove(j,j));
+    EXPECT_TRUE(test_tree_->Remove(j, j));
   }
 
   uint64_t del_depth = test_tree_->GetDepth();
 
   uint64_t num_end_leaves = 0;
-  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr; cur_node = cur_node->right_)
-    num_end_leaves ++;
+  for (BPlusTree<uint64_t, uint64_t>::LeafNode *cur_node = test_tree_->FindMinLeaf(); cur_node != nullptr;
+       cur_node = cur_node->right_)
+    num_end_leaves++;
 
   EXPECT_EQ(num_start_leaves, num_end_leaves);
   EXPECT_EQ(start_depth, del_depth);
@@ -157,12 +164,11 @@ TEST_F(BPlusTreeTests, CompressTest4) {
 
   std::vector<uint64_t> results;
   for (uint64_t i = 1; i < BPlusTree<uint64_t, uint64_t>::LEAF_SIZE / 2; i += 2) {
-    test_tree_->ScanKey(i, &results, [] (uint64_t val) { return true; });
+    test_tree_->ScanKey(i, &results, [](uint64_t val) { return true; });
     EXPECT_EQ(results.size(), 1);
     EXPECT_EQ(results[0], i);
     results.clear();
   }
-
 }
 
 }  // namespace terrier::storage::index
